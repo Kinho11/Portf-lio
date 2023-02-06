c908b569a2f5b80937e5a7b67dc69783c677e0e6
Kinho11 <marcuspaulo.morenomf@gmail.com>
2023-02-06 15:09:28 -0300

    alterando animação

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,16 @@ import { RepositorioProvider } from './Context/RepositorioGitHub/RepositorioGitH
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { Footer } from './components/Footer/Footer';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import AOS from 'aos';
 import "aos/dist/aos.css";
 
 function App() {
 
-  useEffect(()=>{
     AOS.init({
       duration: 1200
     })
-  }, [])
+
 
   return (
     <>
